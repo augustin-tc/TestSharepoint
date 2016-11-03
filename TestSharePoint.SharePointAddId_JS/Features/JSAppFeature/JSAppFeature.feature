05eb2e8fba5d0aea449598f1f96fa4b521e6b6ea
Augustin T. <augustin.tc@live.fr>
2016-11-03 23:13:55 +0100

    added JS app

---
+++ TestSharePoint.SharePointAddId_JS/Features/JSAppFeature/JSAppFeature.feature
@@ -0,0 +1,9 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<feature xmlns:dm0="http://schemas.microsoft.com/VisualStudio/2008/DslTools/Core" dslVersion="1.0.0.0" Id="8a039830-50d0-4ad4-a2cf-3c211b98be1c" description="This is a JS app for SharePoint" featureId="8a039830-50d0-4ad4-a2cf-3c211b98be1c" imageUrl="" solutionId="00000000-0000-0000-0000-000000000000" title="TestSharePoint.SharePointAddId_JS JSAppFeature" version="" deploymentPath="$SharePoint.Project.FileNameWithoutExtension$_$SharePoint.Feature.FileNameWithoutExtension$" xmlns="http://schemas.microsoft.com/VisualStudio/2008/SharePointTools/FeatureModel">
+  <projectItems>
+    <projectItemReference itemId="e2b9aefe-20b8-4082-9746-db7d916f779a" />
+    <projectItemReference itemId="6f1c06b5-b7a3-47af-8f44-654ee763279d" />
+    <projectItemReference itemId="d4b59a3a-6f07-4218-a352-c1f99289d236" />
+    <projectItemReference itemId="005fd9d0-0e91-45da-b8dc-3956912d7c0a" />
+  </projectItems>
+</feature>
---
+++ TestSharePoint.SharePointAddId_JS/Scripts/App.js
@@ -0,0 +1,63 @@
+﻿'use strict';
+
+ExecuteOrDelayUntilScriptLoaded(initializePage, "sp.js");
+
+function initializePage()
+{
+    var context = SP.ClientContext.get_current();
+    var user = context.get_web().get_currentUser();
+    var oWebsite = context.get_web();
+    // This code runs when the DOM is ready and creates a context object which is needed to use the SharePoint object model
+    $(document).ready(function () {
+        getUserName();
+        //getSiteTitle();
+        retrieveWebSite('http://k2/sites/test')
+    });
+
+    function getSiteTitle() {
+       
+        
+        context.load(web);
+        var SiteTitle = web.get_title();
+
+        $('#content').text('Site title : ' + SiteTitle);
+    }
+
+
+    function retrieveWebSite(siteUrl) {
+        
+        
+        context.load(oWebsite);
+
+        context.executeQueryAsync(onQuerySucceeded , onQueryFailed);
+          
+    }
+
+    function onQuerySucceeded(sender, args) {
+        alert('Title: ' + oWebsite.get_title() +
+            ' Description: ' + oWebsite.get_description());
+    }
+
+    function onQueryFailed(sender, args) {
+        alert('Request failed. ' + args.get_message() +
+            '\n' + args.get_stackTrace());
+    }
+
+
+    // This function prepares, loads, and then executes a SharePoint query to get the current users information
+    function getUserName() {
+        context.load(user);
+        context.executeQueryAsync(onGetUserNameSuccess, onGetUserNameFail);
+    }
+
+    // This function is executed if the above call is successful
+    // It replaces the contents of the 'message' element with the user name
+    function onGetUserNameSuccess() {
+        $('#message').text('Hello ' + user.get_title());
+    }
+
+    // This function is executed if the above call fails
+    function onGetUserNameFail(sender, args) {
+        alert('Failed to get user name. Error:' + args.get_message());
+    }
+}
